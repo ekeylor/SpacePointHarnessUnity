6afba87f03edf927f18e497d86240b6d40f234e9
Eric Keylor <ekeylor@gmail.com>
2017-09-04 11:04:30 -0400

    2017-9-4:  Initial commit.  This test harness implements a Tonnetz.

---
+++ Assets/Scripts/BuildManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildManager : MonoBehaviour {
+	public Transform fpp; // first person player
+
+	// Use this for initialization
+	void Start () {
+		#if UNITY_STANDALONE
+		Instantiate(fpp, new Vector3(0, 0.788f, -1.5f), Quaternion.identity);
+		fpp.Find("MainCamera").transform.SetPositionAndRotation(new Vector3(0, 1.5f, 0), Quaternion.identity);
+		#endif
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
